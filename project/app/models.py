7be536f7886587017b163c20e59625bc44acacd6
siddharth <badkursiddharth282@gmail.com>
2024-07-20 23:24:55 +0530

    trial

---
+++ project/app/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Student(models.Model):
+    stu_name=models.CharField(max_length=50)
+    stu_email=models.EmailField()
+    stu_city=models.CharField(max_length=100)
+    def __str__(self):
+        return self.stu_name
